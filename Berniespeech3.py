7840c9e534ddb148a5bde3a1cd41bd8b110398d4
tayloral00 <amelia.l.taylor@vanderbilt.edu>
2020-06-23 11:08:15 -0400

    new files working on generative code, will integrate with Bernie-twitter later

---
+++ Berniespeech3.py
@@ -0,0 +1,133 @@
+training_doc3 = """
+Thank you all very much for being here tonight and thank you for being part of a political revolution which will transform America.
+
+Thank you for being part of a campaign which is not only going to win the Democratic nomination, which is not only going to defeat Donald Trump, the most dangerous president in modern American history, but with your help is going to transform this country and, finally, create an economy and government which works for all Americans, and not just the one percent. Today, I want to welcome you to a campaign which says, loudly and clearly, that the underlying principles of our government will not be greed, kleptocracy, hatred and lies. It will not be racism, sexism, xenophobia, homophobia and religious bigotry. All of that is going to end.
+
+The principles of our government will be based on justice: economic justice, social justice, racial justice and environmental justice. Tonight, I want to welcome you to a campaign which tells the powerful special interests who control so much of our economic and political life that we will no longer tolerate the greed of Wall Street, corporate America and the billionaire class – greed which has resulted in this country having more income and wealth inequality than any other major country on earth.
+
+No. We will no longer stand idly by and allow 3 people in this country to own more wealth than the bottom half of America while, at the same time, nearly 20 percent of our children live in poverty, veterans sleep out on the streets and seniors cannot afford their prescription drugs. We will no longer accept 46 percent of all new income going to the top 1 percent, while millions of Americans are forced to work 2 or 3 jobs just to survive and over half of our people live paycheck to paycheck, frightened to death about what happens to them financially if their car breaks down or their child becomes sick.
+
+Together, we are going to create a political system which is based on the democratic principles of one person - one vote - and end a corrupt system which allows billionaires to buy elections. Yes. We are going to overturn Citizens United and move to public funding of elections.
+Today, we fight for a political revolution.
+
+And tonight, I want to offer a very special thanks to the people of the great state of Iowa. In 2016, this is where the political revolution began. Thank you Iowa.
+
+When I first came here to campaign in 2015 not a whole lot of people knew who I was, nobody took our campaign seriously, and we were at 3 percent in the polls. Further, the ideas that we were talking about then were considered by establishment politicians and mainstream media to be "radical" and "extreme" - ideas, they said, that nobody in America would support.
+
+Raising the minimum wage to a living wage. Too radical. Guaranteeing health care to all as a right, not a privilege. Too radical. Creating up to 15 million jobs by rebuilding our crumbling infrastructure with a one trillion dollar investment. Too radical. Aggressively combatting climate change. Too radical. Reforming our broken criminal justice and immigration systems. Too radical. Not taking money from super PACs and the rich. Too radical. Ending the power of super delegates at the Democratic Convention. Too radical.
+
+Well, a funny thing happened in Iowa over that year. On Caucus Night we didn't win 3% of the vote, we won 50% of the vote and half of the pledged delegates.
+
+And that great start in Iowa led us to win victories in 22 states around the country, 13 million votes, over 1700 delegates at the convention and more votes from young people - black, white, Latino, Asian American and Native American - than Trump and Clinton combined.
+
+And, by the way. Those ideas that we talked about 4 years years ago that seemed so very radical at that time. Well, today, virtually all of those ideas are supported by a majority of the American people and have overwhelming support from Democrats and independents - and they're ideas that Democratic candidates for president to school board are now supporting.
+
+So Iowa, you helped begin the political revolution in 2016 and, with your help on this campaign, we are going to complete what we started here. We're going to turn our vision and our progressive agenda into reality.
+
+Today, as we launch our campaign here in Iowa, we say to the private health insurance companies, whether you like it or not, the United States will join every other major country on earth and guarantee healthcare to all people as a right. All Americans are entitled to go to the doctor when they're sick and not go bankrupt after staying in the hospital. We will no longer accept the absurdity of paying almost twice as much per capita on health care, while we have a lower life expectancy and worse health care outcomes than many other countries.
+
+The goal of health care must be to provide quality care to all in a cost effective way, not tens of billions in profits for the insurance companies and outrageous compensation packages for CEOs. In 2017, the top 65 healthcare CEO’s made $1.7 billion in compensation including: $83.2 million to David Wichmann, the CEO of UnitedHealth Group and 58.7 million to Mark Bertolini, the CEO of Aetna. We need a health care system that invests in disease prevention, doctors, nurses, dentists and rural clinics. We don't a system which makes insurance companies and their CEOs super rich.
+
+Yes. We will pass a Medicare for all single-payer program. Health care is a right. And, by the way, our legislation improves health care for seniors by providing coverage for dental care, hearing aids and eye glasses.
+
+Today, we say to the pharmaceutical industry, that you will no longer charge the American people the highest prices in the world for prescription drugs, the result being that one out of five Americans cannot afford the prescriptions their doctors prescribe. Seniors in this country should not have to cut their pills in half. The outrageous greed of the pharmaceutical industry is going to end. We are going to lower prescription drug prices in this country.
+
+Today, we say to Walmart, the fast food industry and other low wage employers: Stop paying your employees starvation wages.. Yes. We are going to raise the federal minimum wage to a living wage - $15 an hour. Nobody who works 40 hours a week in this country should live in poverty. And yes. We're going to make it easier for people to join unions, not harder.
+
+Four years ago, when we talked about the idea of a $15 an hour minimum wage, it seemed like an impossible dream. Well, since then, I'm happy to tell you that 5 states have passed $15 an hour legislation and, just yesterday, the U.S. House Committee on Labor and Education reported out a bill that will raise the federal minimum wage from $7.25 an hour to $15 an hour. And, I believe, that bill will pass the full House within the month.
+
+And by the way. Today we say to corporate America that artificial intelligence and robotics are not going to be used just to throw workers out on the street. This exploding technology must serve human needs, not just corporate profits.
+
+Today we say to the American people that we will rebuild our crumbling infrastructure: our roads, our bridges, our rail system and subways, our airports, our water systems and wastewater plants - and when we do that we create up to 13 million good paying jobs. And let's be clear. When I talk about infrastructure and clean water we're talking about strengthening clean water laws so that corporate polluters stop poisoning the drinking water that communities in Iowa and across the country rely on.
+
+Today we say to the parents in this country that you and your kids deserve quality, affordable childcare. The children are our future, and they deserve the best possible head start in life with a high quality, universal pre-K program.
+
+Today, we say to our young people that we want you to get the best education that you can, regardless of the income of your family. Good jobs require a good education. That is why we are going to make public colleges and universities tuition free, and substantially lower the outrageous level of student debt that currently exists. America once had the best educated workforce in the world, and we are going to make that happen again.
+
+Today, we say to our senior citizens, that we understand that you cannot live in dignity when you are trying to survive on $13,000 or $14,000 a year in Social Security benefits. My Republican colleagues want to cut Social Security but we have some bad news for them. We're not going to cut Social Security benefits. We're going to expand them.
+
+Today, we say to Donald Trump and the fossil fuel industry that climate change is not a hoax but is an existential threat to our country and the entire planet - and we intend to transform our energy system away from fossil fuel and into energy efficiency and sustainable energy and, in the process, create millions of good paying jobs. All of us have a moral responsibility to make certain that the planet we leave to our children and grandchildren is healthy and habitable.
+
+Today, we say to the prison-industrial-complex that we are going to bring about real criminal justice reform. We are going to end the international embarrassment of having more people in jail than any other country on earth. Instead of spending $80 billion a year on jails and incarceration, we are going to invest in jobs and education for our young people. No more private prisons and detention centers. No more profiteering from locking people up. No more "war on drugs." No more keeping people in jail because they're too poor to afford cash bail.
+
+And by the way, when we talk about criminal justice reform, we're going to change a system in which tens of thousands of Americans every year get criminal records for possessing marijuana, but not one major Wall Street executive went to jail for destroying our economy in 2008 as a result of their greed, recklessness and illegal behavior. No. They didn't go to jail. They got a trillion-dollar bailout.
+
+Today, we say to the American people that instead of demonizing the undocumented immigrants in this country, we're going to pass comprehensive immigration reform and provide a path toward citizenship. We're going to provide legal status to the 1.8 million young people eligible for the DACA program, and develop a humane border policy for those who seek asylum. No more snatching babies from the arms of their mothers.
+
+Today, we say to the top 1 percent and the large profitable corporations in this country - people who have never had it so good -- that under a Bernie Sanders administration we're going to end the massive tax breaks and loopholes that you currently enjoy.
+
+We will no longer accept the absurd situation where large corporations like Amazon, Netflix and General Motors pay nothing in federal income taxes after raking in billions in profits. We will no longer tolerate the situation in which the wealthy and large corporations stash billions in tax havens throughout the world.
+
+Yes, the wealthy and multi-national corporations in this country will start paying their fair share of taxes. We are going to end austerity for working families, and provide some austerity for large, multi-national corporations.
+
+Today, we say to the military-industrial-complex that we will not continue to spend $700 billion a year on the military - more than the next ten nations combined. We're going to invest in affordable housing, we're going to invest in public education, we're going to invest in rebuilding our crumbling infrastructure - not more nuclear weapons and never-ending wars.
+
+Brothers and sisters: We're going to win this election not because we have a super PAC funded by billionaires. We're going to win this election because we will put together the strongest grassroots coalition in the history of American politics.
+
+Donald Trump wants to divide us up by the color of our skin, our country of origin, our gender, our religion and our sexual orientation. We are going to do exactly the opposite. We are going to bring our people together – black, white, Latino, Native American, Asian American, gay and straight, young and old, men and women, native born and immigrant.
+
+We are going to bring our people together for an unprecedented grassroots effort, which, I am happy to tell you, already has over one million people signed up as volunteers.
+
+Brothers and sisters: As someone who represents Vermont, one of the most rural states in the country, let me be very honest with you in saying that the U.S. Congress has, for too long, ignored the many crises facing rural America.
+
+In Iowa, in Vermont and all over this country, we have seen more and more young people leave the small towns they grew up in and love, not because they don’t want to stay, but because there are fewer and fewer jobs that pay a living wage.
+
+We have seen schools, churches and community centers shut down, and once vibrant Main Streets become boarded up and deserted.
+
+In Vermont, Iowa and all across rural America, we have seen family farmers go out of business as the prices they receive for their products decline rapidly and large agri-business corporations and factory farming take over agriculture.
+
+We have seen rural hospitals and nursing homes shut down, and not enough doctors to provide the quality health care that rural American deserves.
+
+Tragically, instead of seeing good jobs, education and health care coming into our rural communities, we are far too often seeing despair and depression - and a terrible increase in suicide and opioid addiction.
+
+Brothers and sisters. We need policies for rural America that represent the needs of working people and farmers, not agri-business and multi-national corporations.
+
+Among many other things that need to be done is for the federal government to enforce anti-trust laws, and I will appoint an Attorney General who will do just that.
+
+It is not acceptable to me that the top four packing companies control more than 80 percent of the beef market, 63 percent of the pork market, and 53 percent of the chicken market.
+
+And these numbers understate the situation.
+
+In many communities, there really is only one buyer, which means food producers are at their mercy. They must use that corporation’s feed and livestock, they must accept that corporation’s costs, and they must accept that corporation’s lower and lower payment rates. In many cases, the farmer doesn’t even own the livestock or supplies -- they are effectively contract employees who are forced to lease everything, and then get paid an inadequate wage for their very hard work.
+
+With the federal government not enforcing antitrust laws, we have seen mergers like the Bayer-Monsanto approved, giving the two largest conglomerates 78 percent of the corn seed market.
+
+Further, instead of protecting family owned farms, federal support for agriculture is skewed toward huge farms. The top 10 percent of farms currently receive 77 percent of all subsidies.
+
+The time is long overdue for the U.S. government to stand with rural America, and that is exactly what I do.
+
+Brothers and sisters. Over the last two years, and before, you and I and millions of Americans have stood up and fought for justice in every part of our society. And we've had some successes.
+
+Together, as billionaires and large corporations have attacked unions, destroyed pensions, deregulated the banks, and slashed wages, we have succeeded in raising the minimum wage to $15 in states and cities across the country. And forced large corporations like Amazon and Disney to do the same. And we have supported teachers who successfully stood up for their kids in strike after strike after strike.
+
+Together, as the forces of militarism have kept us engaged in unending wars, we have stood arm-in-arm to fight back. For the first time in 45 years, we have utilized the War Powers Act to move us forward in ending the horrific Saudi-led war in Yemen.
+
+Together, as so many of our young people have received criminal records for nonviolent offenses, we have fought to end the war on drugs, and have seen state after state decriminalize marijuana, and have seen communities expunge the criminal records of those arrested on these charges.
+
+Let’s be honest: while we have won some victories, our struggles have not always been successful. But I am here to tell you, that because of all the work we have done, we are now on the brink of winning not just an election, but transforming our country.
+
+And let me tell you what that means.
+
+When We are in the White House, we will enact a federal jobs guarantee, to ensure that everyone is guaranteed a stable job. There is more than enough work to be done in this country. Let's do it.
+
+When We are in the White House we will not only end the decline of rural America, but attack the problem of urban gentrification and build the affordable housing we desperately need all across this country.
+
+When We are in the White House, we will move aggressively to end the epidemic of gun violence in this country and pass the common sense gun safety legislation that the overwhelming majority of Americans want. People who should not have guns, will not have guns.
+When We are in the White House, we are going to address not only the disparities of wealth and income that exist overall in our nation, but we will address the racial disparities of wealth and income. We are going to root out institutional racism wherever it exists. Not only will we end voter suppression, we are going to make it easier for people to vote - not harder.
+
+When We are in the White House, we are going to protect a woman’s right to control her own body. That is her decision, not the government's.
+
+Make no mistake about it, this struggle is not just about defeating Donald Trump. This struggle is about taking on the incredibly powerful institutions that control the economic and political life of this country. And I’m talking about Wall Street, the insurance companies, the drug companies, the military-industrial complex, agri-business, the prison-industrial complex, the fossil fuel industry and a corrupt campaign finance system that enables billionaires to buy elections.
+
+These powerful special interests are going to spend a lot of money to try to defeat us. But we have something they don’t have: the power of the people.
+
+Brothers and sisters: We have an enormous amount of work in front of us. But this what I believe. If we stand together, if we don't allow Trump and his friends to divide us up;
+
+If we stand together as black and white, Latino, Asian American and Native American. If we stand together as gay and straight, men and women, native born and immigrant. If we stand together as rural and urban - north, south, east and west.
+
+If we understand that there really is no such thing as blue state or red state, but states throughout the country where working people are struggling to survive.
+
+If we stand together, this country has an extraordinary future. Let's make it happen.
+
+
+"""
